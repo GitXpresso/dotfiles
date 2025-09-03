1bf10d43642f9e6d172bf3a81b7ea70c9a1b307b
William <126926699+GitXpresso@users.noreply.github.com>
2025-09-03 14:14:50 +0000

    Update fedoradotfilesetup.sh

--- fedoradotfilesetup.sh
+++ fedoradotfilesetup.sh
@@ -16,122 +16,57 @@ if grep -qi "Fedora" /etc/*release; then
           2. Enable Default Prompt to \"Y\" instead of \"N\" when installing packages.
           "
           read -p "Pick An Option [1-3]: " pick_an_option1
-          if [[ "$pick_an_option1" == "1" ]]; then
-                       while true; do
-                               clear
-                               read -p "Do want to enable parallel downloads ( allows to install multiple packages simultaneously (yes/no/y/n): " yesorno2
-           if [[ "$yesorno2" == "yes" || "$yesorno2" == "y" ]]; then
-           while true; do
-             clear
-             echo "
-             1. Default Max Parallel Downloads \"3\"
-             2. Set the custom amount of Parallel Downloads allowed
-             "
-             read -p "Pick an option [1-2]: " pick_an_option2
-               if [ "$pick_an_option2" == "1" ]; then
-                 echo "Setting max parallel downloads to 3"
-                 if ! grep "max_parallel_downloads=3" /etc/dnf/dnf.conf; then
-                   sudo echo "max_parallel_downloads=3" >> /etc/dnf/dnf.conf
-                   break
-                 else
-                    echo "max_parrallel_downloads was already added to \"/etc/dnf/dnf.conf\""
-                    return 1
-                 fi
-                 break
-                 elif [ "$pick_an_option2" == "2" ]; then
-                   while true; do
-                     read -p "Set your custom amount of parallel downloads allowed: " pick_an_number
-                       if ! grep "${1,10}" $pick_an_number; then
-                         echo "Not a number try again..."
-                         return 1
-                       else
-                         sudo echo "max_parallel_downloads=$pick_an_number" >> /etc/dnf.conf
-                         break
-                       fi
+            if [[ "$pick_an_option1" == "1" ]]; then
+              while true; do
+               clear
+               read -p "Do want to enable parallel downloads ( allows to install multiple packages simultaneously (yes/no/y/n): " yesorno2
+                if [[ "$yesorno2" == "yes" || "$yesorno2" == "y" ]]; then
+                  while true; do
+                  clear
+                  echo "
+                  1. Default Max Parallel Downloads \"3\"
+                  2. Set the custom amount of Parallel Downloads allowed
+                  "
+                  read -p "Pick an option [1-2]: " pick_an_option2
+                   if [ "$pick_an_option2" == "1" ]; then
+                     echo "Setting max parallel downloads to 3"
+                      if ! grep "max_parallel_downloads=3" /etc/dnf/dnf.conf; then
+                        echo "max_parallel_downloads=3" >> ~/dnf1.tmp
+                        sudo cp ~/dnf1.tmp /etc/dnf/dnf.conf
+                        rm -f ~/dnf1.tmp
+                        break
+                      else
+                       echo "max_parrallel_downloads was already added to \"/etc/dnf/dnf.conf\""
+                       export $?="1"
+                      fi
+                      break
+                   elif [ "$pick_an_option2" == "2" ]; then
+                      while true; do
+                        read -p "Set your custom amount of parallel downloads allowed: " pick_an_number
+                         if echo "$pick_an_number" | grep -q '^[0-9]'; then
+                           echo "max_parallel_downloads=$pick_an_number" >> ~/dnf.tmp
+                           sudo cp ~/dnf.tmp /etc/dnf/dnf.conf
+                           rm ~/dnf.tmp
+                           break
+                         else
+                           echo "Not a number try again..."
+                           sleep 0.2 
+                           clear
+                        fi
                      done
-                    else
+                     else
                         echo "invaild option try again..."
                         sleep 0.2
                         clear
                     fi
+                    done      
+            else
+               echo "invaild option try again..."
+               sleep 0.2
+               clear
+            fi
             done
-
-           else
-              echo "Invaild option try again..."
-              sleep 0.2
-              clear
-           fi
-           done
-           else
-              echo "Invaild option try again..."
-              sleep 0.2
-              clear
-           fi
-         done
-        else
-           echo "Invaild option try again..."
-           sleep 0.2
-           clear
-        fi
-     done
-  else 
-     echo "Invaild option try again..."
-     sleep 0.2
-     clear
-  fi
-  done
-                  echo "
-                  1. Add Fast Repositories ( Allows faster dnf installs. )
-                  2. Enable Default Prompt to \"Y\" instead of \"N\" when installing packages.
-                  "
-                  read -p "Pick An Option [1-3]: " pick_an_option1
-                    if [[ "$pick_an_option1" == "1" ]]; then
-                       while true; do
-                               clear
-                               read -p "Do want to enable parallel downloads ( allows to install multiple packages simultaneously (yes/no/y/n): " yesorno2
-                           if [[ "$yesorno2" == "yes" || "$yesorno2" == "y" ]]; then
-                             while true; do
-                               clear
-                               echo "
-                               1. Default Max Parallel Downloads \"3\"
-                               2. Set the custom amount of Parallel Downloads allowed
-                               "
-                               read -p "Pick an option [1-2]: " pick_an_option2
-                                 if [ "$pick_an_option2" == "1" ]; then
-                                   echo "Setting max parallel downloads to 3"
-                                   if ! grep "max_parallel_downloads=3" /etc/dnf/dnf.conf; then
-                                     sudo echo "max_parallel_downloads=3" >> /etc/dnf/dnf.conf
-                                     break
-                                   else
-                                     echo "max_parrallel_downloads was already added to \"/etc/dnf/dnf.conf\""
-                                     return 1
-                                   fi
-                                   break
-                                 elif [ "$pick_an_option2" == "2" ]; then
-                                 while true; do
-                                   read -p "Set your custom amount of parallel downloads allowed: " pick_an_number
-                                     if ! grep "${1,10}" $pick_an_number; then
-                                       echo "Not a number try again..."
-                                       return 1
-                                     else
-                                       sudo echo "max_parallel_downloads=$pick_an_number" >> /etc/dnf.conf
-                                       break
-                                     fi
-                                  
-                                  done
-                                  else
-                                     echo "Invaild option try again..."
-                                     sleep 0.2
-                                     clear
-                                  fi
-                              done
-                             else
-                                 echo "Invaild option, try again..."
-                                 sleep 0.2
-                                 clear   
-                             fi
-                          done
-             elif [[ "$yesorno1" == "no" || "$yesorno1" == "n" ]]; then
+            elif [[ "$yesorno1" == "no" || "$yesorno1" == "n" ]]; then
                  echo "Not making any changes to dnf.conf"
              else
                  echo "Invalid Option try again..."
@@ -139,4 +74,3 @@ if grep -qi "Fedora" /etc/*release; then
                  clear
              fi
           done
-          fi
