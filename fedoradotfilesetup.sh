401b6ea8e6fafb601e9406fd21ef56e54e20f832
William <126926699+GitXpresso@users.noreply.github.com>
2025-09-05 13:08:52 +0000

    Update fedoradotfilesetup.sh

--- fedoradotfilesetup.sh
+++ fedoradotfilesetup.sh
@@ -6,8 +6,16 @@ is_yes=$(grep -q "defaultyes=True" /etc/dnf/dnf.conf && echo yes || echo no)
 is_delta=$(grep -q "deltarpm=True" /etc/dnf/dnf.conf && echo yes || echo no)
 is_cache=$(grep -q "keepcache=True" /etc/dnf/dnf.conf && echo yes || echo no)
 is_parrell_downloads=$(grep -q "max_parallel_downloads=$number" /etc/dnf/dnf.conf && echo yes || echo no)
+color_status() {
+  if [ "$1" == "yes" ]; then
+    echo "${green}${no_color}"
+  else
+    echo "${red}${no_color}"
+  fi
+}
+
 echo "
-1. Add Fast Repositories (Allows faster dnf installs) [ Available = $is_fast ] 
+1. Add Fast Repositories (Allows faster dnf installs) [ Available = $(color_status $is_fast) ] 
 2. Enable Default Prompt to \"Y\" instead of \"N\" when installing packages [ Available = $is_yes ] 
 3. Enable DeltaRPM (Downloads only the differences between package versions, saving bandwidth) [ Available = $is_delta ] 
 4. Set Keep Cache value to true (Keeps the downloaded packages in cache, useful for reinstalls or debugging) [ Available = $is_cache ]
