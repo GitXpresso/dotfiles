556330bd67b11a8ff3aeef58ed2df230cbd0f15e
William <126926699+GitXpresso@users.noreply.github.com>
2025-09-05 13:01:30 +0000

    Update fedoradotfilesetup.sh

--- fedoradotfilesetup.sh
+++ fedoradotfilesetup.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 start(){
-curl -fsSL https://raw.githubusercontent.com/GitXpresso/dotfiles/refs/heads/main/fedoradotfilesetup2.sh | bash
+number='^[0-9]\+$'
+is_fast=$(grep -q "fastestmirror=True" /etc/dnf/dnf.conf && echo yes || echo no)
+is_yes=$(grep -q "defaultyes=True" /etc/dnf/dnf.conf && echo yes || echo no)
+is_delta=$(grep -q "deltarpm=True" /etc/dnf/dnf.conf && echo yes || echo no)
+is_cache=$(grep -q "keepcache=True" /etc/dnf/dnf.conf && echo yes || echo no)
+is_parrell_downloads=$(grep -q "max_parallel_downloads=$number" /etc/dnf/dnf.conf && echo yes || echo no)
+echo 
+"
+1. Add Fast Repositories (Allows faster dnf installs) [ Available = $is_fast ] 
+2. Enable Default Prompt to \"Y\" instead of \"N\" when installing packages [ Available = $is_yes ] 
+3. Enable DeltaRPM (Downloads only the differences between package versions, saving bandwidth) [ Available = $is_delta ] 
+4. Set Keep Cache value to true (Keeps the downloaded packages in cache, useful for reinstalls or debugging) [ Available = $is_cache ]
+5. Enable Parrell Downloads ( installs multiple packages simultaneously ) [ Available = $is_parrell_downloads ]
+"
 read -p "Select more than one [Main] configuration [e.g. 1 3 or 1,2]: " pick_an_configuration
 choices=$( echo $pick_an_configuration | tr ',' ' ')
 for choice in $choices; do
@@ -146,6 +159,7 @@ if_rpmnon_free=$(dnf repolist enabled | grep rpmfusion-nonfree | echo | echo no)
             echo "$free_packages"
             read -p "Press any key to clear..."
             clear
+            break
           elif [[ "$pick_an_repository" == "2" ]]; then
               echo "Listing all RPM Fusion non-free repository packages."
               clear
@@ -153,6 +167,7 @@ if_rpmnon_free=$(dnf repolist enabled | grep rpmfusion-nonfree | echo | echo no)
               echo "$non_free_packages"
               read -p "Press any key to clear..."
               clear
+              break
           else
             echo "Invalid option, try again."
             sleep 0.2 
